b14bc19f9eba20371c079c8fe2bd8943ef7c85fe
Parker <pp101socials@gmail.com>
2023-06-28 10:18:09 -0400

    Created data

--- assets/data/variables/ARRAYS.js
+++ assets/data/variables/ARRAYS.js
@@ -1,1 +1,21 @@
 // This file is used to hold some arrays
+
+import {
+  USED_CARS,
+  PAINTER,
+  PLUMBERS,
+  PRESSURE_WASHERS,
+  ARTS_AND_CRAFTS,
+  LAWN_CARE,
+} from "../variables/OBJECTS";
+
+const SAMPLES = [
+  USED_CARS,
+  PAINTER,
+  LAWN_CARE,
+  ARTS_AND_CRAFTS,
+  PLUMBERS,
+  PRESSURE_WASHERS,
+];
+
+export { SAMPLES };
--- assets/data/variables/OBJECTS.js
+++ assets/data/variables/OBJECTS.js
@@ -1,1 +1,61 @@
 // This file is used to hold some objects
+
+import Sample from "../classes/Sample";
+
+const USED_CARS = new Sample(
+  "S_001",
+  undefined,
+  "Used Car Dealerships",
+  undefined,
+  undefined,
+  undefined
+);
+const PAINTER = new Sample(
+  "S_002",
+  undefined,
+  "Painters",
+  undefined,
+  undefined,
+  undefined
+);
+const LAWN_CARE = new Sample(
+  "S_003",
+  undefined,
+  "Lawn Care",
+  undefined,
+  undefined,
+  undefined
+);
+const ARTS_AND_CRAFTS = new Sample(
+  "S_004",
+  undefined,
+  "Arts and Crafts",
+  undefined,
+  undefined,
+  undefined
+);
+const PLUMBERS = new Sample(
+  "S_005",
+  undefined,
+  "Plumbers",
+  undefined,
+  undefined,
+  undefined
+);
+const PRESSURE_WASHERS = new Sample(
+  "S_006",
+  undefined,
+  "Pressure Washers",
+  undefined,
+  undefined,
+  undefined
+);
+
+export {
+  USED_CARS,
+  PAINTER,
+  PLUMBERS,
+  PRESSURE_WASHERS,
+  ARTS_AND_CRAFTS,
+  LAWN_CARE,
+};
