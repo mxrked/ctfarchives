6ccba6ae7b81a6511da6c15e1b5524e5e7e48181
Parker <pp101socials@gmail.com>
2023-06-28 10:18:47 -0400

    Created file

---
+++ assets/cdns/CDNImgs.js
@@ -0,0 +1,15 @@
+/**
+ *
+ *  This is the CDNImgs
+ *
+ */
+
+import { CDNImgReturn } from "./CDNReturns";
+
+const PLACEHOLDER_IMG = CDNImgReturn(
+  "placeholders",
+  "placeholder-orange",
+  "jpg"
+);
+
+export { PLACEHOLDER_IMG };
